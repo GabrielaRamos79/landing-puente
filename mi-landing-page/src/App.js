24f60380a2b8e9674f8ce752d886c7c08fdb92e2
GabrielaRamos79 <gabyramos@me.com>
2024-08-02 19:55:42 +0200

    First Commit

---
+++ mi-landing-page/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import '../images/ff2e987dbc2df60b444c012e41913400.gif'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
