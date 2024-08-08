addac63b97cafd5e99f6e8d1083721191ab5d071
GabrielaRamos79 <gabyramos@me.com>
2024-08-08 14:01:43 +0200

    Second commit

--- mi-landing-page/src/App.js
+++ mi-landing-page/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import '../images/ff2e987dbc2df60b444c012e41913400.gif'
+import logo from './images/ff2e987dbc2df60b444c012e41913400.gif'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Somos el puente</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          tu agencia<br>
+          </br>de publicidad<br>
+          </br> y comunicación 360º
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hacemos las cosas diferentes desde el otro lado<br>
+          </br>del branding: publicidad, branded content, diseño<br>
+          </br>y estrategia, ¡con un <b>punch de flow creativo</b>!<br>
+          </br>
+        </p>
+          <p>Si quieres revolucionar tu marca y llevarla hasta el<br></br>éxito, es el momento de cruzar al otro lado.<br>
+          </br></p>
+          <p>¿Quieres saber qué podemos hacer por ti?
         </p>
         <a
           className="App-link"
@@ -14,7 +29,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Escríbenos
         </a>
       </header>
     </div>
