1cb312fd77f80571f6430a8d91e5bb1804dbf785
GabrielaRamos79 <gabyramos@me.com>
2024-09-29 21:16:11 +0200

    EmailJS configuration Added

--- mi-landing-page/src/components/ContactForm/ContactForm.js
+++ mi-landing-page/src/components/ContactForm/ContactForm.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 import "./ContactForm.css";
 import estrella from "../../images/details/estrella.png";
 // import textura2 from "../../images/details/textura2.png";
 import flecha from "../../images/details/Flecha.png";
 
 function ContactForm() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_usxjseg', 'template_jv0ugsg', form.current, 'eJSf75rO8BXA5nivD')
+      .then((result) => {
+          console.log(result.text);
+          alert('Mensaje enviado con éxito');
+      }, (error) => {
+          console.log(error.text);
+          alert('Hubo un error al enviar el mensaje');
+      });
+  };
   return (
-    <div id="contactForm" className="container contact-form my-5">
+    <div id="contactForm" className="container custom-container my-5">
       <h1 className="form-title">VAMOS AL LÍO!</h1>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="form-group">
           <label htmlFor="name">Nombre</label>
           <div className="input-wrapper">
-            <input type="text" id="name" className="form-input" />
+            <input type="text" id="name"  name="from_name" className="form-input" />
             {/* <img src={estrella} alt="star icon" className="star-icon" /> */}
           </div>
         </div>
         <div className="form-group">
           <label htmlFor="email">Mail</label>
           <div className="input-wrapper">
-            <input type="email" id="email" className="form-input" />
+            <input type="email" id="email" name="user_email" className="form-input" />
             <img src={estrella} alt="star icon" className="star-icon" />
           </div>
         </div>
@@ -30,11 +45,12 @@ function ContactForm() {
             {/* <img src={estrella} alt="star icon" className="star-icon" /> */}
           </div>
         </div>
-        <div className="form-group">
+        
+        <div className="form-group2">
           <label htmlFor="message">Haznos un teaser sobre qué necesitas</label>
-          <textarea id="message" rows="4" className="form-input"></textarea>
+          <textarea id="message" name="message" rows="4" className="form-input"></textarea>
         </div>
-        <div className="form-group">
+        <div className="form-group2">
           <h2>Ready para que tu marca empiece a petarlo?</h2>
           <div className="send-wrapper">
             <button type="submit" className="send-button">
